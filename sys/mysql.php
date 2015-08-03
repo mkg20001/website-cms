aec41008811bb04dc52479e4388e9d0b994de226
mkg20001 <mkg20001@gmail.com>
2015-08-03 11:24:07 +0200

    Updated

--- sys/mysql.php
+++ sys/mysql.php
@@ -10,21 +10,12 @@ if ($_GET["against"]=="true" and URI=="install") {
 unlink(CONFIG);
 header("Location: http://".DOMAIN."/install");
 }
-
     die("ERROR Accessing MySQL database  <a href=\"http://".DOMAIN."/install?against=true\">Reconfigure</a>");
-
 } else {
-//mysqli_query("SET CHARACTER SET 'utf8'");
-//mysqli_query("USE ".$C["sql"]["database"].";");
 $GLOBALS["C"]["sql"]["i"]=$mysqli;
 }
 //Executes MySQL Queries
-function domy($input,$db="") {
-if ($db == "") {
-$db=$C["sql"]["database"];
-}
-//mysqli_query("SET CHARACTER SET 'utf8'");
-//mysqli_query("USE $db;");
+function domy($input) {
 return mysqli_query($GLOBALS["C"]["sql"]["i"],$input);
 }
 
